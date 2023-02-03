d0cd6d9d0b5a7ff6fc868af635382631ac755622
giuseppe sorrentino <giuseppesorrentino11290@gmail.com>
2023-02-03 15:50:28 +0100

    Initial commit

---
+++ src/main/java/co/develhope/firstapi2/controllers/FactorialController.java
@@ -0,0 +1,25 @@
+package co.develhope.firstapi2.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/factorial")
+public class FactorialController {
+    @GetMapping
+    public String getDefaultMessage(){
+        return "Calcoliamo il Fattoriale ";
+    }
+
+    @GetMapping("/{n}")
+    public Integer getFactorial(@PathVariable (name = "n") Integer n ){
+        Integer result = n;
+        while (n>1 ){
+            result *= (--n);
+
+        }
+        return result;
+    }
+}
